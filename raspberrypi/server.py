eace3bcce96a3d0541f48073d9730cd14c1fe7d3
Marco Pashkov <marcopashkov@gmail.com>
2013-03-29 22:31:15 -0700

    MP: ajax-ified the page

--- raspberrypi/server.py
+++ raspberrypi/server.py
@@ -12,27 +12,27 @@ arduino = serial.Serial("/dev/ttyACM0", 9600)
 def runCommand(motor, speed, steps, direction, style):
     """send the message to the arduino and return the execution code"""
     message = "{0};".format(",".join([motor, speed, steps, direction, style]))
-    print "running: {0}".format(message)
     code = arduino.write(message)
-    print "code: {0}".format(code)
     return code
 
 ### webserver
 @app.route("/")
 def index():
     """render the control page"""
-    message = []
-    while arduino.inWaiting() > 0:
-        message.append(arduino.readline())
-    return render_template('index.html', message=message)
+    return render_template('index.html')
 
 @app.route("/call", methods=['POST'])
 def call():
     """parse any messages and run them on the arduino"""
-    ### send to arduino
+    ### send command to arduino
     args = ["motor", "speed", "steps", "direction", "style"]
-    runCommand(*[request.form.get(v) for v in args])
-    return redirect(url_for("index"))
+    code = runCommand(*[request.form.get(v) for v in args])
+    
+    ### gather messages return from arduino
+    message = ["return code: {0}".format(code),]
+    while arduino.inWaiting() > 0:
+        message.append(arduino.readline())
+    return render_template('message.html', message=message)
 
 if __name__ == "__main__":
     ### start the server
