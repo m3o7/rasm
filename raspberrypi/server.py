c0c4112f4ae5b31e2b2f0e55178c0c0acd23561a
Marco Pashkov <marcopashkov@gmail.com>
2013-03-29 22:01:50 -0700

    MP: first rudementary web interface

--- raspberrypi/server.py
+++ raspberrypi/server.py
@@ -1,9 +1,34 @@
-from flask import Flask
-app = Flask(__name__)
+### imports
+from flask import Flask, render_template, request, redirect, url_for
+import serial
 
+### flask setup
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+### arduino connection setup
+arduino = serial.Serial("/dev/ttyACM0", 9600)
+
+def runCommand(motor, speed, steps, direction, style):
+    message = "{0};".format(",".join([motor, speed, steps, direction, style]))
+    print "running: {0}".format(message)
+    arduino.write(message)
+
+### webserver
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    message = []
+    while arduino.inWaiting() > 0:
+        message.append(arduino.readline())
+    return render_template('index.html', message=message)
+
+@app.route("/call", methods=['POST'])
+def call():
+    ### send to arduino
+    args = ["motor", "speed", "steps", "direction", "style"]
+    runCommand(*[request.form.get(v) for v in args])
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
+    ### start the server
     app.run()
